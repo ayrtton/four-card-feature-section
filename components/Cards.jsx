b112ac33c3fb437ffc4cab38173a9b89746c1d94
Ayrton <ayrtton.vinicius@gmail.com>
2024-01-30 18:14:15 -0300

    Style the header

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -2,7 +2,7 @@ function Cards() {
     return (
         <main>
             <div id="header">
-                <h1>Reliable, efficient delivery Powered by Technology</h1>
+                <h1><span>Reliable, efficient delivery</span>Powered by Technology</h1>
                 <p>Our Artificial Intelligence powered tools use millions of project data points 
                 to ensure that your project is successful</p>
             </div>
